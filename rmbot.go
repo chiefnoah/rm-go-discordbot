87d853d2e9963c1ab8ebe31a4143574e9b6e4edf
Noah Pederson <pedersonnoah@gmail.com>
2016-09-14 09:35:13 -0500

    More basic structure stuff

--- rmbot.go
+++ rmbot.go
@@ -1,9 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"github.com/chiefnoah/rm-go-discordbot/config"
+	"log"
+	"time"
 )
 
 func main() {
-	fmt.Println("Test")
+
+	cfg := config.LoadConfig()
+
+	discord, err := discordgo.New(cfg.AppConfig.AuthToken)
+	if err != nil {
+		log.Fatal("Unable to authenticate with discord")
+		return
+	}
+	err = discord.Open()
+	if err != nil {
+		log.Fatal("Unable to open connection: ", err)
+		return
+	}
+	defer discord.Close()
+
+
+
+
+	for {
+		discord.UpdateStatus(0, "moderating")
+		time.Sleep(10 * time.Second)
+	}
+
 }
