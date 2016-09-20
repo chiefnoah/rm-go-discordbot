7533b66025d443e42f1820fd6d97911fe65e79c1
Kevin <kminttech@gmail.com>
2016-09-19 23:22:06 -0500

    Attempted Change to nickname code

--- commands/parsecommands.go
+++ commands/parsecommands.go
@@ -259,7 +259,9 @@ func setGoddess(s *discordgo.Session, m *discordgo.Message, extraArgs []string, 
 				log.Print("Unable to update user: ", err)
 				return
 			}
-			err = s.GuildMemberNickname(g.ID, newUser.ID, name + "-Bot")
+			nickMessage := "/nick " + name + "-Bot"
+			message, err := s.ChannelMessageSend(m.ChannelID, nickMessage)
+			//err = s.GuildMemberNickname(g.ID, newUser.ID, name + "-Bot")
 			if err != nil {
 				log.Print("Unable to set nickname: ", err)
 			}
