72c31465c05c9412906ea7c6a4d5f139cc1ac3c4
Noah Pederson <pedersonnoah@gmail.com>
2016-10-29 00:20:19 -0500

    Added more database init statements. Can now pull available managed roles. Need to limit based on current group membership

--- database/database.go
+++ database/database.go
@@ -8,7 +8,9 @@ import (
 )
 
 const (
-	initTables = "CREATE TABLE IF NOT EXISTS `ManagedRole` (`RoleID` TEXT UNIQUE, `Name` TEXT NOT NULL,`Description`TEXT);"
+	initTables = "CREATE TABLE IF NOT EXISTS `ManagedRole` (`RoleID` TEXT UNIQUE, `Name` TEXT NOT NULL,`Description`TEXT);" +
+		"CREATE TABLE IF NOT EXISTS `User` (`JoinedAt` TEXT, `Nick` TEXT, `username` TEXT, `Bot` INTEGER DEFAULT 0, `DiscordID` TEXT, `NickLastChanged` INTEGER DEFAULT 0, `Admin` INTEGER DEFAULT 0);" +
+		"CREATE TABLE IF NOT EXISTS `ManagedRolePermissions` (`ManagedRoleID` TEXT NOT NULL, `DiscordRole` TEXT NOT NULL);" //This is a bridging table between discord's roles and the managed roles. It basically just allows us to limit which Discord roles are allowed to join a managed role
 )
 
 var db *sql.DB
@@ -33,6 +35,9 @@ func End() {
 	db.Close()
 }
 
+/*
+	Returns the roles a user has permission to join.
+ */
 func GetManagedRoles(userID string) ([]ManagedRole, error) {
 	//TODO: only pull roles that the user has permissions to join
 	if db == nil {
@@ -42,9 +47,9 @@ func GetManagedRoles(userID string) ([]ManagedRole, error) {
 		}
 		db = tdb
 	}
-	rows, err := db.Query("SELECT RoleID, Name, Description FROM MangedRoles")
+	rows, err := db.Query("SELECT RoleID, Name, Description FROM ManagedRole INNTER JOIN ManagedRolePermissions ON RoleID = ManagedRolePermissions.ManagedRoleID")
 	if err != nil {
-		log.Print("Unable to retrieve all Managed roles")
+		log.Printf("Unable to retrieve all Managed roles for userID: %s\nError:%s",userID, err)
 		return nil, errors.New("Unable to retrieve managed roles")
 	}
 	output := make([]ManagedRole, 0)
@@ -62,6 +67,7 @@ func GetManagedRoles(userID string) ([]ManagedRole, error) {
 	}
 	return output, nil
 }
+
 
 
 type ManagedRole struct {
